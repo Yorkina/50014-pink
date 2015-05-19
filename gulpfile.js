d569d8d77d39395337851992d4778a0a47f51d51
Natalia Yorkina <yorkinan@gmail.com>
2015-05-19 18:21:58 +0300

    add auto pref

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,16 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('less', function() {
 	gulp.src('./less/style.less')
 		.pipe(less())
 		.pipe(gulp.dest('./css'))
+		.pipe(autoprefixer({
+          browsers: ['last 2 versions'],
+          cascade: false
+        }))
 		.pipe(reload({
 			stream: true
 		}));
