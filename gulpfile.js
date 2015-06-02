28f3e854982b4d5131f3465950007711bd2db0e7
Natalia Yorkina <yorkinan@gmail.com>
2015-06-02 19:06:35 +0300

    причесала

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,31 @@
-var gulp = require('gulp');  
+var gulp = require('gulp');
 var less = require('gulp-less');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('less', function() {
-gulp.src('./less/style.less')
-	.pipe(less())
-	.pipe(autoprefixer())
-	.pipe(gulp.dest('./css'))
-	.pipe(reload({
-	stream: true
-}));
+    gulp.src('./less/style.less')
+        .pipe(less())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./css'))
+        .pipe(reload({
+            stream: true
+        }));
 });
 
 gulp.task('watch', function() {
-		gulp.watch('./less/**/*.less', ['less']);
+    gulp.watch('./less/**/*.less', ['less']);
 });
 
-gulp.task('serve', ['less'], function () {
+gulp.task('serve', ['less'], function() {
 
-	browserSync.init({
-		server: '.'
-	});
+    browserSync.init({
+        server: '.'
+    });
 
-	gulp.watch("./less/**/*.less", ['less']);
-	gulp.watch("Pink/*.html").on('change', reload);
+    gulp.watch("./less/**/*.less", ['less']);
+    gulp.watch("Pink/*.html").on('change', reload);
 });
 
 gulp.task('default', ['less', 'serve']);
