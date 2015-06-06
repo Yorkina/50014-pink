f1ecde6acbb20d639cbe59547055619b337e65e9
Natalia Yorkina <yorkinan@gmail.com>
2015-06-06 18:50:53 +0300

    add new coord.

--- src/js/icon__for-map.js
+++ src/js/icon__for-map.js
@@ -2,14 +2,14 @@
 ymaps.ready(function() {
     var myMap;
     myMap = new ymaps.Map('footer__map--y', {
-            center: [59.938667, 30.323073],
+            center: [59.936453, 30.32283],
             zoom: [15],
             controls: []
         }),
     myMap.behaviors.disable('scrollZoom');
     myMap.controls.add('zoomControl');
-    myPlacemark = new ymaps.Placemark([59.938667, 30.323073], {
-        hintContent: '', // различная инфа при наведении на метку
+    myPlacemark = new ymaps.Placemark([59.936453, 30.32283], {
+        hintContent: 'Жмакабельно', // различная инфа при наведении на метку
         balloonContent: ''// различная инфа при нажатии на метку
     }, {
         iconLayout: 'default#image',
