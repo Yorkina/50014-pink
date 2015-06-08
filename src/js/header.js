a80d38c01b3c7416fc8f169c3402a696032e4279
Natalia Yorkina <yorkinan@gmail.com>
2015-06-08 15:27:30 +0300

    add tap

--- src/js/header.js
+++ src/js/header.js
@@ -4,7 +4,7 @@
     var btnMenu = document.querySelector(".header__btn-menu");
     var headerHeadline = document.querySelector(".header__head-line");
 
-    btnMenu.addEventListener("click", function(event) {
+    btnMenu.addEventListener("tap", function(event) {
         event.preventDefault();
         if (!this.classList.contains("header__btn-menu--closed")) {
             this.classList.add("header__btn-menu--closed");
