da25b1398139c042d03f8a588d4b328e08683ad6
Natalia Yorkina <yorkinan@gmail.com>
2015-06-10 02:48:12 +0300

    add correct coord. for point

--- src/js/icon__for-map.js
+++ src/js/icon__for-map.js
@@ -1,21 +1,17 @@
-;
 ymaps.ready(function() {
-    var myMap;
-    myMap = new ymaps.Map('footer__map--y', {
-            center: [59.936453, 30.32283],
-            zoom: [15],
-            controls: []
-        }),
-    myMap.behaviors.disable('scrollZoom');
-    myMap.controls.add('zoomControl');
-    myPlacemark = new ymaps.Placemark([59.936453, 30.32283], {
-        hintContent: 'Жмакабельно', // различная инфа при наведении на метку
-        balloonContent: ''// различная инфа при нажатии на метку
+    var o;
+    o = new ymaps.Map("footer__map--y", {
+        center: [59.938554, 30.32248],
+        zoom: [15],
+        controls: []
+    }), o.behaviors.disable("scrollZoom"), o.controls.add("zoomControl"), 
+    myPlacemark = new ymaps.Placemark([59.938563, 30.32160], {
+        hintContent: "Жмакабельно",
+        balloonContent: ""
     }, {
-        iconLayout: 'default#image',
-        iconImageHref: '../src/img/svg/map-marker.svg',
+        iconLayout: "default#image",
+        iconImageHref: "../src/img/svg/map-marker.svg",
         iconImageSize: [40, 40],
         iconImageOffset: [0, 0]
-    });
-    myMap.geoObjects.add(myPlacemark);
+    }), o.geoObjects.add(myPlacemark)
 });
