bfbffed960bf784fdc5d4fe69e307cd7ad7a83c6
Natalia Yorkina <yorkinan@gmail.com>
2015-06-11 23:52:08 +0300

    upd scroll func

--- src/js/desktop-menu.js
+++ src/js/desktop-menu.js
@@ -1,24 +1,24 @@
 ;
 (function() {
 var linkNav = document.querySelectorAll('[href^="#nav"]'),
-    V = 0.5;  // скорость, может иметь дробное значение через точку
+    V = 0.5;
 for (var i = 0; i < linkNav.length; i++) {
   linkNav[i].addEventListener('click', function(e) {
     e.preventDefault();
-    var w = window.pageYOffset,  // прокрутка
-        hash = this.href.replace(/[^#]*(.*)/, '$1');  // id элемента, к которому нужно перейти
-        t = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
+    var scroll = window.pageYOffset,
+        hash = this.href.replace(/[^#]*(.*)/, '$1');
+        position = document.querySelector(hash).getBoundingClientRect().top,
         start = null;
-    requestAnimationFrame(step);  // подробнее про функцию анимации [developer.mozilla.org]
+    requestAnimationFrame(step);
     function step(time) {
       if (start === null) start = time;
       var progress = time - start,
-          r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
-      window.scrollTo(0,r);
-      if (r != w + t) {
+          roll = (position < 0 ? Math.max(w - progress/V, scroll + position) : Math.min(scroll + progress/V, scroll + position));
+      window.scrollTo(0,roll);
+      if (roll != scroll + position) {
         requestAnimationFrame(step)
       } else {
-        location.hash = hash  // URL с хэшем
+        location.hash = hash;
       }
     }
   }, false);
