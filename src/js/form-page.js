1f5ab04e8ae5c9063713f9fe6eacab806cc4b677
Natalia Yorkina <yorkinan@gmail.com>
2015-06-17 01:29:51 +0300

    add local storage, but it does not work correctly

--- src/js/form-page.js
+++ src/js/form-page.js
@@ -3,36 +3,78 @@
     if (!("FormData" in window)) {
         return;
     }
-
     var form = document.querySelector(".form-content__form");
-
     form.addEventListener("submit", function(event) {
         event.preventDefault();
-
         var data = new FormData(form);
-
         requestAjax(data, function(response) {
             console.log(response);
         });
     });
 
     function requestAjax(data, fn) {
-        var test = new XMLHttpRequest();
-        var time = (new Date()).getTime();
+            var test = new XMLHttpRequest();
+            var time = (new Date()).getTime();
 
-        test.open("post", "http://simonenko.su/academy/echo?" + time, true);
+            test.open("post", "http://simonenko.su/academy/echo?" + time, true);
 
-        test.addEventListener("readystatechange", function() {
-            if (test.readyState == 4) {
-                fn(test.responseText);
-            }
-        });
+            test.addEventListener("readystatechange", function() {
+                if (test.readyState == 4) { //НЕ работает так как хочется=)
+                    fn(test.responseText);
+                    localStorage.clear();
+                }
+            });
 
-        test.send(data);
+            test.send(data);
+        }
+//ЛОКАЛ СТОРАЖ//
+    if (document.querySelector(".form-content__form")) {
+        console.info('Форма обнаружена!');
+        addLocalStorage();
     }
+
+    function addLocalStorage() {
+        if (window.localStorage) {
+            console.info('Форма обнаружена 6!');
+            var form = document.querySelector(".form-content__form");
+            var saveParam = form.querySelectorAll("[name]") -1;//убрать -1
+            for (var i = 0; i < saveParam.length; i++) {
+
+                getState(saveParam[i]);
+                setState(saveParam[i]);
+
+            }
+        }
+    };
+
+    function getState(saveParam) {
+
+        var name = saveParam.getAttribute('name');
+        saveParam.value = localStorage.getItem(name) || '';
+
+    };
+
+    function setState(saveParam) {
+        console.info('Форма обнаружена ---!');
+        var name = saveParam.getAttribute('name');
+        saveParam.addEventListener('keyup', function() {
+            console.info('Форма обнаружена ===!');
+            var value = this.value;
+
+            if (!value) {
+                value = '';
+            }
+
+            localStorage.setItem(name, value);
+
+        });
+    };
+
+//ФОТО//
     if ("FileReader" in window) {
         var area = document.querySelector(".travel-photo");
         var adder = document.querySelector(".travel-photo__images");
+
         area.querySelector("#upload__photo").addEventListener("change", function() {
 
             var files = this.files;
@@ -42,20 +84,15 @@
             }
         });
 
-
-
         function preview(file) {
             if (file.type.match(/image.*/)) {
                 var reader = new FileReader();
-
                 reader.addEventListener("load", function(event) {
-
 
                     var figure = document.createElement("figure");
                     var img = document.createElement("img");
                     var cancel = document.createElement("a");
                     var caption = document.createElement("figcaption");
-
 
                     img.src = event.target.result;
                     img.alt = file.name;
@@ -80,45 +117,45 @@
             }
         }
     }
-
+//ДОБАВЛЕНИЕ ПОЛЕЙ ВВОДА //
     function travelersBox(options) {
-        var curCount = 0,
-            wrap = document.querySelector(options.wrap),
-            self = this;
+            var curCount = 0,
+                wrap = document.querySelector(options.wrap),
+                self = this;
 
-        this.init = function(count) {
-            curCount = +count;
-            wrap.innerHTML = "";
-            for (var i = 1; i <= curCount; i++) {
-                var row = getHtmlTemplate(i);
-                wrap.innerHTML += row;
-            };
-            setEvents();
-        }
-
-        this.change = function(count) {
-            var count = +count;
-            return count != curCount ? (count > curCount ? addRows(count) : removeRows(count)) : false;
-        }
-
-        var addRows = function(count) {
-            curCount = +count;
-            if (wrap.children.length < curCount) {
-                var row = getHtmlTemplate(curCount);
-                wrap.innerHTML += row;
+            this.init = function(count) {
+                curCount = +count;
+                wrap.innerHTML = "";
+                for (var i = 1; i <= curCount; i++) {
+                    var row = getHtmlTemplate(i);
+                    wrap.innerHTML += row;
+                };
                 setEvents();
             }
-        }
 
-        var removeRows = function(count) {
-            curCount = +count;
-            wrap.lastChild.remove();
-        }
+            this.change = function(count) {
+                var count = +count;
+                return count != curCount ? (count > curCount ? addRows(count) : removeRows(count)) : false;
+            }
 
-        var getHtmlTemplate = function(num) {
-            var num = +num,
-                curCount = curCount < num ? num : curCount,
-                tpl = '<div class="travelers__row">\
+            var addRows = function(count) {
+                curCount = +count;
+                if (wrap.children.length < curCount) {
+                    var row = getHtmlTemplate(curCount);
+                    wrap.innerHTML += row;
+                    setEvents();
+                }
+            }
+
+            var removeRows = function(count) {
+                curCount = +count;
+                wrap.lastChild.remove();
+            }
+
+            var getHtmlTemplate = function(num) {
+                var num = +num,
+                    curCount = curCount < num ? num : curCount,
+                    tpl = '<div class="travelers__row">\
                             <div class="travelers__names">\
                                 <header>\
                                     <span>№</span>\
@@ -135,32 +172,32 @@
                                 <input type="text" name="traveler-nick[' + num + ']" tabindex="13" value="" placeholder="Ну как же без этого!" required>\
                             </div>\
                         </div>';
-            return tpl;
-        }
+                return tpl;
+            }
 
-        var setEvents = function() {
-            var rows = wrap.querySelectorAll("button.travelers__name--delete");
-            for (var i = rows.length - 1; i >= 0; i--) {
-                rows[i].addEventListener('click', function(event) {
-                    event.preventDefault();
-                    this.parentNode.parentNode.remove();
-                    curCount = +wrap.children.length;
-                    options.counter.value = curCount;
-                    if (curCount > 0)
-                        updateNums(curCount);
-                });
-            };
+            var setEvents = function() {
+                var rows = wrap.querySelectorAll("button.travelers__name--delete");
+                for (var i = rows.length - 1; i >= 0; i--) {
+                    rows[i].addEventListener('click', function(event) {
+                        event.preventDefault();
+                        this.parentNode.parentNode.remove();
+                        curCount = +wrap.children.length;
+                        options.counter.value = curCount;
+                        if (curCount > 0)
+                            updateNums(curCount);
+                    });
+                };
+            }
+            var updateNums = function(count) {
+                var rows = wrap.children;
+                for (var i = 0, c = 1; i <= rows.length - 1, c <= count; i++, c++) {
+                    rows[i].querySelector(".travelers__form-number").innerText = c;
+                    rows[i].querySelector(".travelers__names input[type=text]").name = "traveler-name[" + c + "]";
+                    rows[i].querySelector(".travelers__nick input[type=text]").name = "traveler-nick[" + c + "]";
+                };
+            }
         }
-        var updateNums = function(count) {
-            var rows = wrap.children;
-            for (var i = 0, c = 1; i <= rows.length - 1, c <= count; i++, c++) {
-                rows[i].querySelector(".travelers__form-number").innerText = c;
-                rows[i].querySelector(".travelers__names input[type=text]").name = "traveler-name[" + c + "]";
-                rows[i].querySelector(".travelers__nick input[type=text]").name = "traveler-nick[" + c + "]";
-            };
-        }
-    }
-
+//СЧЕТЧИК//
     function Counter(options) {
         var wrap = document.querySelector(options.wrap);
         var countElem = document.querySelector(options.box);
@@ -184,7 +221,7 @@
                 travelers.change(countElem.value)
             }
         });
-//// ПРОВЕРКА НА ВВОД
+//ПРОВЕРКА НА ВВОД//
         countElem.oninput = function(e) {
             var pattern = /^[^A-Za-zА-Яа-я/]+$/g;
             if (this.value.match(pattern)) {
@@ -234,7 +271,7 @@
 
     calcTravalers.setCount(2);
 
-    /////////Изменение даты инпутов 
+//ИЗМЕНЕНИЕ ДАТЫ ИНПУТОВ//
     if (document.querySelector(".form-content__form")) {
         var tripLasting = document.getElementById('trip-lasting');
         var btnMinus = document.querySelector('.travel-duration__btn-minus');
@@ -299,15 +336,16 @@
             departure.value = a.getFullYear() + '-' + month + '-' + day;
         }
         tripLasting.addEventListener('change', function() {
-                if(tripLasting.value) {
-                    plusDate(tripLasting.value);
-                }
-            });
+            if (tripLasting.value) {
+                plusDate(tripLasting.value);
+            }
+        });
         checkout.addEventListener('change', function() {
-                if(checkout.value) {
-                    plusDate(tripLasting.value);
-                }
-            });
+            if (checkout.value) {
+                plusDate(tripLasting.value);
+            }
+        });
+
         function changeNumbers(number, el) {
             if ((parseInt(el.value) + number) < 1) {
                 el.value = 1;
@@ -321,27 +359,28 @@
             }
         }
     }
-     //////JQ календарик для инпутов с типом дата
-     window.isMobile = function() {
+//JQ КАЛЕНДАРИК ДЛЯ ИНПУТОВ С ТИПОМ ДАТА//
+    window.isMobile = function() {
         var check = false;
-        (function(a,b){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+        (function(a, b) {
+            if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true
+        })(navigator.userAgent || navigator.vendor || window.opera);
         return check;
-        };
+    };
     $(function() {
-        if(!isMobile()) {
-        $('input[type=date]').datepicker({
-         dateFormat: 'yy-mm-dd',
-         minDate: "-30",
-         onSelect: function() {
-            if(checkout.value) {
-                plusDate(tripLasting.value);
-              }
-            }
-        });
+        if (!isMobile()) {
+            $('input[type=date]').datepicker({
+                dateFormat: 'yy-mm-dd',
+                minDate: "-90",
+                onSelect: function() {
+                    if (checkout.value) {
+                        plusDate(tripLasting.value);
+                    }
+                }
+            });
         }
     });
-    ///////////конец календарика
-    ////////конец изменения даты в инпутах
+//ЗАКРЫТИЕ ПОПАПОВ//
 
 
     var failure = document.querySelector(".failure-popup");
